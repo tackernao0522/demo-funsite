d47523ee960684d2e4dada23ed8c11aebc5039e2
takaki <cheap_trick_magic@yahoo.co.jp>
2021-04-24 12:06:57 +0900

    laravel 入れ直し

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,19 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('stripe_id')->unique()->nullable();
-            $table->integer('status')->default(0)->comment("有料会員(1)か無料会員(0)か");
-            $table->enum('role', ['member', 'admin'])->default('member');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
-        DB::table('users')->insert(['id' => 1, 'name' => 'Admin', 'email' => 'admin@example.com', 'password' => bcrypt('password'), 'role' => 'admin']);
-        DB::table('users')->insert(['id' => 2, 'name' => 'User', 'email' => 'user@example.com', 'password' => bcrypt('password')]);
     }
 
     /**
--- server/resources/views/home.blade.php
+++ server/resources/views/home.blade.php
@@ -5,30 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ Auth::user()->name }}さん、こんにちは！</div>
+                <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
                     @endif
-                    <div>
-                        <ul class="list-group">
-                            <li class="list-group-item">
-                                <a href="{{route('user.info')}}">ユーザー情報</a>
-                            </li>
-                            <li class="list-group-item">
-                                <a href="{{route('user.payment')}}">お支払い情報について</a>
-                            </li>
-                            <li class="list-group-item">
-                                <form action="{{route('logout')}}" method="POST">
-                                    @csrf
-                                    <button id="btn-logout" class="btn btn-danger">ログアウト</button>
-                                </form>
-                            </li>
-                        </ul>
-                    </div>
+
+                    You are logged in!
                 </div>
             </div>
         </div>
