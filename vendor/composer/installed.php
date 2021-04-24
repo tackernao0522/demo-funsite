408a75a932edf31d7d051bd54004b4723655f6b8
takaki <cheap_trick_magic@yahoo.co.jp>
2021-04-24 12:25:19 +0900

    StripeのKEY設定等

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'fc041f775630831fb5b5453aac06e554075f8d12',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fc041f775630831fb5b5453aac06e554075f8d12',
+    ),
+    'stripe/stripe-php' => 
+    array (
+      'pretty_version' => 'v7.77.0',
+      'version' => '7.77.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6724447481f6fb8c2e714165e092adad9ca470a',
+    ),
+  ),
+);
