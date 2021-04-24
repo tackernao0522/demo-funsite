a19ac19972909a407e9430fc589fa1eec8538572
takaki <cheap_trick_magic@yahoo.co.jp>
2021-04-24 13:36:25 +0900

    Stripeクレカ登録機能の実装

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +7,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/user/payment', 'User\PaymentController@getCurrentPayment')->name('user.payment');
+Route::get('/user/payment/form', 'User\PaymentController@getPaymentForm')->name('user.payment.form');
+Route::post('/user/payment/store', 'User\PaymentController@storePaymentInfo')->name('user.payment.store');
+Route::post('/user/payment/destroy', 'User\PaymentController@deletePaymentInfo')->name('user.payment.destroy');
--- server/webpack.mix.js
+++ server/webpack.mix.js
@@ -12,6 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js').vue()
+mix.js('resources/js/payment.js', 'public/js').vue()
 mix.sourceMaps().js('node_modules/popper.js/dist/popper.js', 'public/js').sourceMaps()
    .sass('resources/sass/app.scss', 'public/css')
    .version();
